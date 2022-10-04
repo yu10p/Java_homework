2983990af72e377f6630c36e989224e8ad0e4de0
yu10p <eason31003@gmail.com>
2022-10-04 13:42:05 +0800

    test version_1

--- hw1/src/hw1/Sample1.java
+++ hw1/src/hw1/Sample1.java
@@ -3,6 +3,7 @@ package hw1;
 public class Sample1 {
 	public static void main (String[] args)
     {
+		//test
         System.out.print("歡迎使用Java!");
         System.out.print("開始使用Java吧!");
     }
